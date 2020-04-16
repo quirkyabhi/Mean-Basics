a84bcc0a2e67be59084ba7acf532ed5a67d1d869
Abhijeet Pansari <abhijeetpansari123@gmail.com>
2020-04-16 17:36:58 +0530

    function added

--- typescriptprac/Helloworld.js
+++ typescriptprac/Helloworld.js
@@ -1,2 +1,23 @@
 var student = "shivam";
 console.log(student);
+var students = [101, "abhi"];
+console.log(students);
+function printName(name) {
+    console.log("Hi " + name);
+}
+function pname(name) {
+    return "hi " + name;
+}
+printName("Yash");
+console.log(pname("abhi"));
+var day = 1;
+switch (day) {
+    case 0:
+        console.log("It is sunday");
+        break;
+    case 1:
+        console.log("It is sunday");
+        break;
+    default:
+        break;
+}
--- typescriptprac/Helloworld.ts
+++ typescriptprac/Helloworld.ts
@@ -1,2 +1,26 @@
 let student= "shivam";
 console.log(student);
+let students:[number,string]=[101,"abhi"];
+console.log(students);
+
+function printName(name:string) {
+    console.log("Hi "+ name);
+}
+
+function pname(name:string): string {
+    return "hi "+ name
+}
+printName("Yash")
+console.log(pname("abhi"))
+let day:number=1;
+switch (day) {
+    case 0:
+        console.log("It is sunday")
+        break;
+    case 1:
+        console.log("It is sunday")
+        break;
+
+    default:
+        break;
+}
